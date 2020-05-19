176d8e9b60463a58faa71323b875ffc50def586b
sehermahmud <seher.mahmud@gmail.com>
2020-05-19 07:05:49 +0600

    Added

---
+++ 0-syntax.js
@@ -0,0 +1,16 @@
+const firstName = "Seher";
+const lastname = 'Mahmud'
+
+const val = 42
+
+const arr = [
+  'string',
+  42,
+  function() {console.log('hi') },
+]
+
+arr[2]()
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i])
+  }
